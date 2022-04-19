8b9cefb57446a1524d8a9f0285be2eb38e1a8d02
SAISREE02 <102043169+SAISREE02@users.noreply.github.com>
2022-04-19 21:52:31 +0530

    Add files via upload

---
+++ UserController.java
@@ -0,0 +1,47 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+import com.User;
+
+@WebServlet("/add")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDao userDao;
+
+	public void init() {
+		userDao = new UserDao();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		register(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("register.jsp");
+	}
+
+	private void register(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+
+		userDao.saveUser(user);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("register-success.jsp");
+		dispatcher.forward(request, response);
+	}
+}
